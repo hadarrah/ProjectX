dbb5358e6d4013cb63d23acacf1a3f92aa1303e6
hadarrah <33288273+hadarrah@users.noreply.github.com>
2017-12-22 13:35:48 +0200

    gggg

--- Server/src/action/EchoServer.java
+++ Server/src/action/EchoServer.java
@@ -27,7 +27,7 @@ import ocsf.server.*;
  */
 public class EchoServer extends AbstractServer 
 {
-	//hadar habulbul
+	//aviram habulbul
   //Class variables *************************************************
   
   /**
