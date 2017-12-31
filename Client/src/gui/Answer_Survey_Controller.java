8288d2d8ce4691c371a5e7df03f2eaafc7fffc85
dell <dell@D1234567>
2017-12-31 15:38:53 +0200

    s4

--- Client/src/gui/Answer_Survey_Controller.java
+++ Client/src/gui/Answer_Survey_Controller.java
@@ -1,42 +1,89 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class Answer_Survey_Controller implements ControllerI,Initializable {
-public RadioButton R3,R1;
-public  ToggleGroup group;
-public HBox root;
-@Override
-public void initialize(URL location, ResourceBundle resources) {
-	/*
-	ToggleGroup group = new ToggleGroup();
-	 
-    // Radio 1: Male
- 
-    R1.setToggleGroup(group);
-    R1.setSelected(true);
+public RadioButton R1_1,R1_2;
+public Button Back_to_main,submit_survey;
 
-    // Radio 3: Female.
-    
-    R3.setToggleGroup(group);
 
-    HBox root = new HBox();
-    root.setPadding(new Insets(10));
-    root.setSpacing(5);
-    root.getChildren().addAll(   R1, R3);
+
+public void back_to_main(ActionEvent event) throws IOException {
+
+
+	 Parent menu;
+	 menu = FXMLLoader.load(getClass().getResource(main.fxmlDir+ "Main_menu_F.fxml"));
+	// to_Client.setController(new Managment_Controller());
+	 Scene win1= new Scene(menu);
+	 Stage win_1= (Stage) ((Node) (event.getSource())).getScene().getWindow();
+	 win_1.setScene(win1);
+	 win_1.show();
+}
+
+public void form_submit(ActionEvent event) throws IOException {
 	
-	// TODO Auto-generated method stub
-	*/
+	  Alert alert = new Alert(AlertType.INFORMATION);
+      alert.setTitle("Thank you!");
+      alert.setContentText("Your answers were submitted - have a GOOD day!");
+
+      Optional<ButtonType> result = alert.showAndWait();
+      if (result.get() == ButtonType.OK)
+      {
+      	System.out.println("after thr con");
+      }
+        
+	
+	
+}
+
+
+
+
+
+
+
+
+@Override
+public void initialize(URL location, ResourceBundle resources) 
+{
+	ToggleGroup group = new ToggleGroup();
+
+	RadioButton rb1 = new RadioButton("Size 9");
+	rb1.setToggleGroup(group);
+	rb1.setSelected(true);
+
+	RadioButton rb2 = new RadioButton("Size 10");
+	rb2.setToggleGroup(group);
+	 
+	RadioButton rb3 = new RadioButton("Size 11");
+	rb3.setToggleGroup(group);
+	
+	rb1.setVisible(true);
+	rb2.setVisible(true);
 }
 
 }
+
+
 
 
 
